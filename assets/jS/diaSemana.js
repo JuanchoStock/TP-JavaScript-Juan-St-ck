d9746a46cb00690318954b03ddf4cd54215e1120
Juan <stockjuan@gmail.com>
2024-05-03 10:49:03 -0300

    Se creo estructura punto 4, diaSemana.

--- assets/jS/diaSemana.js
+++ assets/jS/diaSemana.js
@@ -1,3 +1,34 @@
 //Escriba un programa que pida al usuario un número del 1 al 7 y luego
 //muestre el día de la semana correspondiente (1 para "Lunes", 2 para
 //"Martes", etc.).
+
+let dia= prompt (parseFloat('Ingrese un numero para saber que día de la semana es: '));
+
+
+switch (dia) {
+    case valor1:
+        console.log('Lunes')
+      [break;]
+    case valor2:
+        console.log('Martes')
+      [break;]
+      case valor3:
+        console.log('Miercoles')
+      [break;]
+    case valor4:
+        console.log('Jueves')
+      [break;]
+      case valor5:
+        console.log('Viernes')
+      [break;]
+    case valor6:
+        console.log('Sábado')
+      [break;]
+      case valor7:
+        console.log('Domingo')
+      [break;]
+    
+    default:
+      console.log('El valor ingresado no es valido.')
+      [break;]
+  }
