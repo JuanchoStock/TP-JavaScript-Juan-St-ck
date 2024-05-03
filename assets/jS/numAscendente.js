901db7569027f8b61a9dc1ad8016f69a8f7837cc
Juan <stockjuan@gmail.com>
2024-05-03 10:24:20 -0300

    Se finalizo punto 3, orden número Ascendente.

--- assets/jS/numAscendente.js
+++ assets/jS/numAscendente.js
@@ -1,14 +1,39 @@
 //Escribe un programa que pida al usuario tres números y luego los muestre
 //en orden ascendente.
-prompt (parseFloat('Ingrese 3 numero para descubrir el orden Ascendente'))
+//prompt (parseFloat('Ingrese 3 numero para descubrir el orden Ascendente'))
 
 let num1= prompt (parseFloat('Ingrese el primer Número'));
 let num2= prompt (parseFloat('Ingrese el segundo Número'));
 let num3= prompt (parseFloat('Ingrese el tercer Número'));
 
-if (num1>num2 && num1>num3 && num2>num3){
-    console.log('El orden ascendente de los numeros es: ', num1 , num2 , num3)
+if (num1>num2 && num1>num3){
+    console.log('El numero 1 ingresado es el mayor.')
+    if (num2>num3){
+        console.log('El orden de los numeros Ascendentes es: ', num3 ,num2 , num1)
+    }
+    else{
+        console.log('El orden de los numeros Ascendentes es: ', num2 ,num3 , num1)
+    }
+}
+else if(num2>num1 && num2>num3){
+    console.log('El numero 2 ingresado es el mayor.')
+    if(num1>num3){
+        console.log('El orden de los números Ascendentes es: ', num3, num1, num2)
+    }
+    else{
+        console.log('El orden de los números Ascendentes es: ', num1, num3, num2)
+    }
+}
+else if(num3>num1 && num3>num2){
+    console.log('El numero 3 ingresado es el mayor.')
+    if(num1>num2){
+        console.log('El orden de los números Ascendentes es: ', num2, num1, num3)
+    }
+    else{
+        console.log('El orden de los números Ascendentes es: ', num1, num2, num3)
+    }
 }
 else{
-    ('Cualquier valor')
+    console.log('Valores ingresados no válidos.')
 }
+    
