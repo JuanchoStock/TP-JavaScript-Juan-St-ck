0013b2be3e208f2ba3cc0f0575abd6f46f2449d2
Juan <stockjuan@gmail.com>
2024-05-03 11:16:00 -0300

    Se finalizo el punto 2 Par/impar.

--- assets/jS/parImpar.js
+++ assets/jS/parImpar.js
@@ -1,3 +1,14 @@
 //Escriba un programa que pida al usuario un número y luego muestre si es
 //par o impar.
 
+let num = parseInt(prompt('Ingrese un número para saber si es Par o Impar'));
+
+if (num % 2 == 0){
+    console.log('El número ingresado es par')
+}
+    else if(num % 2 !== 0){
+        console.log('El número ingresado es Impar.')
+    }
+    else{
+        console.log('El valor ingresado es incorrecto.')
+    }
