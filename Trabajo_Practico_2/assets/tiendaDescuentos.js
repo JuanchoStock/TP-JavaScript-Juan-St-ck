41290273967daa2811ab1439e6f53d27216ed5aa
Juan <stockjuan@gmail.com>
2024-05-10 11:00:59 -0300

    Se completo estructura punto 2, falta validaciones

--- Trabajo_Practico_2/assets/tiendaDescuentos.js
+++ Trabajo_Practico_2/assets/tiendaDescuentos.js
@@ -6,3 +6,22 @@
 // aplicarse al precio total para luego mostrar en pantalla un mensaje que lo
 // confirme. De lo contrario, indicar que no se superó la cantidad o el precio, según
 // corresponda.
+
+let cant_art = parseInt(prompt('Ingrese la cantidad de articulos que lleva: '))
+let precio_uni = parseInt(prompt('Ingrese el precio unitario del articulos que lleva: '))
+
+pre_total = (cant_art * precio_uni);
+
+console.log('El total a pagar es de: ', '$'+pre_total)
+
+switch (true) {
+    case pre_total >= 20000 && cant_art >=10:
+        pre_desc = pre_total - (pre_total * 0.15)
+        console.log('El precio total a pagar CON descuento es de: ', '$'+pre_desc)
+        break;
+
+    default:
+        
+        break;
+}
+
