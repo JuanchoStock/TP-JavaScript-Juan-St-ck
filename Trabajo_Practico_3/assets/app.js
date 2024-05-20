5154c534fcc65e4f81c099bfaa880e388bb4de09
Juan <stockjuan@gmail.com>
2024-05-20 12:21:16 -0300

    se finalizo programa y validaciones.

--- Trabajo_Practico_3/assets/app.js
+++ Trabajo_Practico_3/assets/app.js
@@ -8,12 +8,7 @@ proporcionando un nombre para la tarea.
 lista.
 
 4. Eliminar Tarea: El usuario podrá eliminar una tarea de la lista.
-
-// Expresion regular que valida que no contenga espacios en blanco al principio o al final ni que este vacio.
-const regex = /^\S+$/
 */
-
-let menu = prompt(' - Menú de Opciones - \n 1 - Listar Tareas.\n 2 - Agregar Tarea. \n 3 - Editar Tarea.\n 4 - Eliminar Tarea.\n 0 - Salir')
 
 
 // 1 - Agregar una tarea.
@@ -46,7 +41,7 @@ function editarTarea(nomTar, nuevaTar){
 
 // 4 - 
 function eliminarTarea(nomTar){
-    const indice = tarea.indexOf (nomTar);
+    const indice = tareas.indexOf (nomTar);
     if (indice !== -1){
         tareas.splice(indice, 1);
         alert('Se eliminó tarea.')
@@ -56,14 +51,70 @@ function eliminarTarea(nomTar){
     }
 }
 
-/*
-menu()
-while (n =! 0){
-    menu()
-    if(n === 0){
-        console.log('Salir')
+let tareas = [];
+/* Expresion regular que valida que no contenga espacios en blanco al principio o al final ni que este vacio.*/
+const regex = /^\S+$/
+/* Menu de opciones */
+let menu = prompt(' - Menú de Opciones - \n 1 - Agregar Tarea.\n 2 - Listar Tareas. \n 3 - Editar Tarea.\n 4 - Eliminar Tarea.\n 0 - Salir')
+
+
+while (menu !== "0"){
+    if(menu.trim() === "" || isNaN(menu) || parseInt(menu)< 0 || parseInt(menu)>4)
+        {
+            alert('La Opción ingresada No es valida. Intente denuevo.')
+        }
+    
+    else{
+        switch(parseInt(menu)){
+            case 1:
+                nomTar = prompt('Ingrese el nombre de la Tarea: ');
+                if (regex.test(nomTar) && isNaN(nomTar)){
+                    agregarTareas(nomTar);
+                }else{
+                    alert('El Nombre de la tarea no puede ser vacio o un número o símbolo.');
+                }
+                break;
+                case 2:
+                    if (tareas.length > 0){
+                        listarTareas();
+                    }else{
+                        alert('La lista esta vacía, ingrese una tarea.')
+                    }
+                    break;
+                    case 3:
+                        if (tareas.length > 0){
+                            nomTar = prompt('Ingrese nombre de la tarea que quiere editar: ');
+                            const indice = tareas.indexOf(nomTar);
+                            if (indice !== -1){
+                                nuevaTar = prompt('Ingrese el nuevo nombre de la tarea.');
+                                if (nuevaTar.trim() !== ""){
+                                    editarTarea(nomTar, nuevaTar);
+                                }else{
+                                    alert('En nombre de la tarea no puede ser vacío.')
+                                }
+                            }else{
+                                alert('No se encontro esa Tarea en la lista.')
+                            }
+                        }else{
+                            alert('No hay tareas por editar.');
+                        }
+                        break;
+
+                        case 4:
+                            if(tareas.length > 0){
+                                nomTar = prompt ('Ingrese el nombre de la tarea que desea eliminar.');
+                                if (nomTar.trim() !== "" && regex.test(nomTar) && isNaN(nomTar)){
+                                    eliminarTarea(nomTar);
+                                }else{
+                                    alert('El nombre de la tarea que desea eliminar no puede estar vacío.')
+                                }
+                            }else{
+                                alert('La lista esta vacía, no hay tareas para eliminar.');
+                            }
+                            break;
+                            default:
+                                break;
+        }
     }
-    else if(n<1 || n>5){
-        console.log('Opción ingresada Incorrecta.')
-    }
-}*/
+    menu = prompt(' - Menú de Opciones - \n 1 - Agregar Tareas.\n 2 - Listar Tarea. \n 3 - Editar Tarea.\n 4 - Eliminar Tarea.\n 0 - Salir')
+}
