37207055f656725d175b772afa1d3e5ebaf6bc89
Juan <stockjuan@gmail.com>
2024-05-20 09:38:18 -0300

    Se corrigió formato de menú.

--- Trabajo_Practico_3/assets/app.js
+++ Trabajo_Practico_3/assets/app.js
@@ -8,19 +8,32 @@ proporcionando un nombre para la tarea.
 lista.
 
 4. Eliminar Tarea: El usuario podrá eliminar una tarea de la lista.
+
+// Expresion regular que valida que no contenga espacios en blanco al principio o al final ni que este vacio.
+const regex = /^\S+$/
 */
 
+let menu = prompt(' - Menú de Opciones - \n 1 - Listar Tareas.\n 2 - Agregar Tarea. \n 3 - Editar Tarea.\n 4 - Eliminar Tarea.\n 0 - Salir')
+//Arreglo que voy a recorrer.
+let lista = [];
+
+//1 Agregar una tarea.
+list.push(1);
+
+//2 Listar el arreglo.
+
+for (let i = 0; i < list.length; i++) {
+    // Haciendo uso del contador "i", vamos recorriendo las posiciones del array.
+    console.log(list[i])
+}
+/*
+menu()
 while (n =! 0){
-    console.log(' - Menú de Opciones - ')
-    console.log(' 1 - Listar Tareas.')
-    console.log(' 2 - Agregar Tarea.')
-    console.log(' 3 - Editar Tarea.')
-    console.log(' 4 - Eliminar Tarea.')
-    console.log(' 0 - Salir.')
+    menu()
     if(n === 0){
         console.log('Salir')
     }
     else if(n<1 || n>5){
         console.log('Opción ingresada Incorrecta.')
     }
-}
+}*/
