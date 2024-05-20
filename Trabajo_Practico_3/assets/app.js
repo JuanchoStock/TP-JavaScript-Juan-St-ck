c8c4dab28a921f3756200f670babffefd71d0a2a
Juan <stockjuan@gmail.com>
2024-05-20 10:08:54 -0300

    Se creo funciones para editar y eliminar Tarea.

--- Trabajo_Practico_3/assets/app.js
+++ Trabajo_Practico_3/assets/app.js
@@ -14,16 +14,15 @@ const regex = /^\S+$/
 */
 
 let menu = prompt(' - Menú de Opciones - \n 1 - Listar Tareas.\n 2 - Agregar Tarea. \n 3 - Editar Tarea.\n 4 - Eliminar Tarea.\n 0 - Salir')
-//Arreglo que voy a recorrer.
-let lista = [];
 
-//1 Agregar una tarea.
+
+// 1 - Agregar una tarea.
 function agregarTareas(nomTar){
     tareas.push(nomTar)
     //console.log para probar
 }
 
-//2 Listar el arreglo.
+// 2 - Listar el arreglo.
 function listarTareas(){
     let msj = "";
     for (let i = 0; i < tareas.length; i++) {
@@ -33,6 +32,29 @@ function listarTareas(){
     alert(msj);
 }
 
+// 3 - Editar una Tarea.
+function editarTarea(nomTar, nuevaTar){
+    const indice = tareas.indexOf(nomTar);
+    if(indice !== -1){
+        tareas[indice] = nuevaTar;
+        alert("Se editó nombre de Tarea.");
+    }
+    else{
+        alert('La tarea que desea editar, no se encuentra en la lista.')
+    }
+}
+
+// 4 - 
+function eliminarTarea(nomTar){
+    const indice = tarea.indexOf (nomTar);
+    if (indice !== -1){
+        tareas.splice(indice, 1);
+        alert('Se eliminó tarea.')
+    }
+    else{
+        alert('La tarea que desea eliminar, no se encuentra en la lista')
+    }
+}
 
 /*
 menu()
